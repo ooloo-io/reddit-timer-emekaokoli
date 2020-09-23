e1dcebbba274b8317d44926f8d5a7ced2b2bab84
Emeka Okoli <emekaokoli00@gmail.com>
2020-09-23 10:57:54 +0100

    corrected lint error

--- src/Header.test.js
+++ src/Header.test.js
@@ -4,20 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import * as renderer from 'react-test-renderer';
 import Header from './components/Header';
 
-// test('render header component', () => {
-//   expect(Footer()).toBeDefined();
-// });
-
-// test('there is a new flavor idea', () => {
-//   expect(fetchNewFlavorIdea()).toBeDefined();
-// });
-
-// test('renders header component', () => {
-//   const { getByText } = render(<Header />);
-//   const linkElement = getByText(/how-it-works/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 describe('Checks header component', () => {
   it('renders correctly', () => {
     const tree = renderer
