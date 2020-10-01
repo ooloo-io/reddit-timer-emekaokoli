8bd82ed294cd3766167b096cc32f2b1a1f15fe42
Emeka Okoli <emekaokoli00@gmail.com>
2020-10-01 22:26:37 +0100

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { HashLink as Link } from 'react-router-hash-link';
-import styled from 'styled-components';
-import GetLogo from './images/elements-logo.svg';
+import {
+  Container,
+  Links,
+  LiTag,
+  Logo,
+  Navlink,
+  NavTag,
+  UlTag
+} from './common/header/Header.style';
 
 export default function Nav() {
   return (
     <>
-      <HeadTag>
+      <Container>
         <Navlink to="/">
-          <Logo src={GetLogo} alt={GetLogo} />
+          <Logo alt={Logo} />
         </Navlink>
         <NavTag>
           <UlTag>
@@ -17,64 +22,16 @@ export default function Nav() {
               <Navlink to="/search/javascript">Search</Navlink>
             </LiTag>
             <LiTag data-testid="howitworks">
-              <Links smooth to="/#how-it-works">
-                How it works
-              </Links>
+              <Links to="/#how-it-works">How it works</Links>
             </LiTag>
             <LiTag data-testid="about">
-              <Links smooth to="/#about">
+              <Links to="/#about">
                 About
               </Links>
             </LiTag>
           </UlTag>
         </NavTag>
-      </HeadTag>
+      </Container>
     </>
   );
 }
-const NavTag = styled.nav`
-  margin: 0.2rem;
-`;
-const HeadTag = styled.header`
-  margin: 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-const UlTag = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-`;
-const LiTag = styled.li`
-  margin: 0.6rem;
-  padding: 0.5rem;
-  display: inline-block;
-  align-content: flex-start;
-`;
-const Logo = styled.img`
-  width: 150px;
-  height: 36px;
-  object-fit: contain;
-  margin: 0.8rem;
-  cursor: pointer;
-`;
-const Links = styled(Link)`
-  text-decoration: none;
-  transition: all 0.3s ease 0s;
-  -webkit-transition: all 0.3s ease 0s;
-  -moz-transition: all 0.3s ease 0s;
-  -ms-transition: all 0.3s ease 0s;
-  -o-transition: all 0.3s ease 0s;
-  &:hover {
-    transition: all 0.3s ease 0s;
-    -webkit-transition: all 0.3s ease 0s;
-    -moz-transition: all 0.3s ease 0s;
-    -ms-transition: all 0.3s ease 0s;
-    -o-transition: all 0.3s ease 0s;
-    color: green;
-  }
-`;
-const Navlink = styled(NavLink)`
-  text-decoration: none;
-`;
--- src/components/Search.js
+++ src/components/Search.js
@@ -26,6 +26,7 @@ const App = styled.div`
   text-align: center;
   justify-content: center;
   vertical-align: middle;
+  height: 100%;
 `;
 const Input = styled.input`
   // width: 50vmin;
