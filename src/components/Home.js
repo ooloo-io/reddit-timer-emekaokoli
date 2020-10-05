55ec22284a92f0c2faa503fd200f014786b55664
Emeka Okoli <emekaokoli00@gmail.com>
2020-10-05 17:42:50 +0100

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
 import HeroSection from './HeroSection';
-import { theme } from './theme.style';
 
 export default function HomeScreen() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <HeroSection />
-      </ThemeProvider>
+      <HeroSection />
     </>
   );
 }
