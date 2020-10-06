66f0905df70aa39d2c3c07e8d97438a3ffe983a5
Emeka Okoli <emekaokoli00@gmail.com>
2020-10-06 12:49:45 +0100

    update

--- src/Header.test.js
+++ src/Header.test.js
@@ -2,30 +2,37 @@ import { cleanup, render } from '@testing-library/react';
 // import { renderer } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
 
 afterEach(cleanup);
 it('containts search link', async () => {
   const { getByTestId } = render(
-    <Router>
-      <Header />
-    </Router>,
+    <ThemeProvider>
+      <Router>
+        <Header />
+      </Router>
+    </ThemeProvider>,
   );
   expect(getByTestId('search')).toHaveTextContent('Search');
 });
 it('containts how it works in the link', async () => {
   const { getByTestId } = render(
-    <Router>
-      <Header />
-    </Router>,
+    <ThemeProvider>
+      <Router>
+        <Header />
+      </Router>
+    </ThemeProvider>,
   );
   expect(getByTestId('howitworks')).toHaveTextContent('How it works');
 });
 it('containts about link', async () => {
   const { getByTestId } = render(
-    <Router>
-      <Header />
-    </Router>,
+    <ThemeProvider>
+      <Router>
+        <Header />
+      </Router>
+    </ThemeProvider>,
   );
   expect(getByTestId('about')).toHaveTextContent('About');
 });
