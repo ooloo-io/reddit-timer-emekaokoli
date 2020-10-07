27985e740d0d919fef65a753d5489fc6f8d7360c
Emeka Okoli <emekaokoli00@gmail.com>
2020-10-07 11:46:36 +0100

    updated tests

--- src/Header.test.js
+++ src/Header.test.js
@@ -1,43 +1,18 @@
 import { cleanup, render } from '@testing-library/react';
 // import { renderer } from '@testing-library/react';
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
+import App from './App';
 
 afterEach(cleanup);
 it('containts search link', async () => {
-  const { getByTestId } = render(
-    <Router>
-      <Header />
-    </Router>,
-  );
+  const { getByTestId } = render(<App />);
   expect(getByTestId('search')).toHaveTextContent('Search');
 });
 it('containts how it works in the link', async () => {
-  const { getByTestId } = render(
-    <Router>
-      <Header />
-    </Router>,
-  );
+  const { getByTestId } = render(<App />);
   expect(getByTestId('howitworks')).toHaveTextContent('How it works');
 });
 it('containts about link', async () => {
-  const { getByTestId } = render(
-    <Router>
-      <Header />
-    </Router>,
-  );
+  const { getByTestId } = render(<App />);
   expect(getByTestId('about')).toHaveTextContent('About');
 });
-// describe('Checks header component', () => {
-//   it('renders correctly', () => {
-//     const tree = renderer
-//       .create(
-//         <Router>
-//           <Header />
-//         </Router>,
-//       )
-//       .toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
