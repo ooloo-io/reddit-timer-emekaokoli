0aa346c487679922b4e4c09512d89519c2d70add
Emeka Okoli <emekaokoli00@gmail.com>
2020-10-14 11:59:41 +0100

    corrections

--- src/components/common/infosection/infoSection.style.js
+++ src/components/common/infosection/infoSection.style.js
@@ -4,23 +4,24 @@ import styled from 'styled-components';
 const Div = styled.div``;
 const section = styled.section``;
 export const Main = styled(Div)`
-   //min-height: 100vmin;
-   width: 110vmin;
-   flex-direction: column;
-   justify-content: center;
-   align-content: center;
-   margin: 0 auto;
- `;
+  //min-height: 100vmin;
+  width: ${(props) => props.theme.font.size.vmin};
+  flex-direction: column;
+  justify-content: center;
+  align-content: ${(props) => props.theme.font.size.center};
+  margin: ${(props) => props.theme.font.size.marginCenter};
+`;
 
 export const Container = styled(Div)`
-  // min-height: 100vh;
+  // min-height: 100vh;a
   display: flex;
   flex-direction: column;
 `;
 
 export const Section = styled(section)`
   text-align: center;
-  margin: 3rem 0 7rem 0;
+  margin: ${(props) => props.theme.font.size.marginCenter} 0 
+  ${(props) => props.theme.font.size.marginCenter} 0;
 `;
 
 export const Links = styled(Link)`
@@ -30,7 +31,7 @@ export const Links = styled(Link)`
 
 export const H4 = styled.h4`
   font-family: ${(props) => props.theme.font.family.default};
-  font-size: 1.5rem;
+  font-size: ${(props) => props.theme.font.size.onePointFive};
   font-weight: normal;
   font-stretch: normal;
   font-style: normal;
@@ -38,7 +39,7 @@ export const H4 = styled.h4`
   letter-spacing: normal;
   text-align: left;
   color: ${(props) => props.theme.color.dark};
-  margin: 0.5rem;
+  margin: ${(props) => props.theme.font.size.zeroPointFive};
 `;
 
 export const Ul = styled.ul``;
@@ -52,7 +53,7 @@ export const Li = styled.li`
 
 export const Paragraph = styled.p`
   font-family: ${(props) => props.theme.font.family.default};
-  font-size: 1rem;
+  font-size: ${(props) => props.theme.font.size.default};
   font-weight: normal;
   font-stretch: normal;
   font-style: normal;
@@ -60,6 +61,7 @@ export const Paragraph = styled.p`
   letter-spacing: normal;
   color: ${(props) => props.theme.color['gray-base']};
   text-align: left;
-  margin: 1rem 0 1rem 0;
-  padding: 1rem;
+  margin: ${(props) => props.theme.font.size.default} 0 
+  ${(props) => props.theme.font.size.default} 0;
+  padding: ${(props) => props.theme.font.size.default};
 `;
